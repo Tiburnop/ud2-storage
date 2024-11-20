5b44d62bf4530a7a190cce58aaf1d6f9e0497347
Tiburnop <rafael.galvan.pomares@gmail.com>
2024-11-20 19:09:14 +0100

    Ejercicio 2 Rafael

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
 
+
 Route::apiResource('hello', HelloWorldController::class);
+
+use App\Http\Controllers\JsonController;
+
+Route::get('/json', [JsonController::class, 'index']);
+Route::post('/json', [JsonController::class, 'store']);
+Route::get('/json/{filename}', [JsonController::class, 'show']);
+Route::put('/json/{filename}', [JsonController::class, 'update']);
+Route::delete('/json/{filename}', [JsonController::class, 'destroy']);
+
